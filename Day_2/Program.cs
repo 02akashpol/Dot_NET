8715ece5a6f85726adde3d2fa9e0ff94a1d30b13
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-12-02 09:31:55 +0530

    Assignment files are uploaded.

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,65 +1,137 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee
+namespace Assignment01
 {
+    class Assignemt01Final
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Employee o1 = new Employee("Amol", 123465, 10);
+            Employee o2 = new Employee("Amol", 123465);
+            Employee o3 = new Employee("Amol");
+            Employee o4 = new Employee();
+
+            Console.WriteLine(o1.getNetSalary());
+
+            Console.WriteLine(o1.EmpNo);
+            Console.WriteLine(o2.EmpNo);
+            Console.WriteLine(o3.EmpNo);
+
+            Console.WriteLine(o3.EmpNo);
+            Console.WriteLine(o2.EmpNo);
+            Console.WriteLine(o1.EmpNo);
+
+            Console.ReadLine();
+        }
+    }
     class Employee
     {
-        private static int empNo;
-        private String empName;
-        private decimal basic;
-        private int deptNo;
- 
-        public Employee(String empName, decimal basic, int deptNo)
-        {
-            this.empName = empName;
-            this.basic = basic;
-            this.deptNo = deptNo;
-            empNo++;
-        }
+        private string empName;
+        //private int empNo;
+        private decimal basicSalary;
+        private short deptNo;
+        static int id = 0;
 
-        public Employee(String empName, decimal basic)
-        {
-            this.empName = empName;
-            this.basic = basic;
-            empNo++;
-        }
 
-        public Employee(String empName)
+        static Employee()
         {
-            this.empName = empName;
-            empNo++;
+            //empNo = 0;
         }
+        //public Employee(string empName = "null1", decimal basicSalary = 0, short deptNo = 0)
+        //{
+        //    id++;
+        //    //this.empName = empName;
+        //    this.EmpName = EmpName;
+        //    //this.basicSalary = basicSalary;
+        //    //this.deptNo = deptNo;
+        //    this.BasicSalary = basicSalary;
+        //    this.DeptNo = deptNo;
+        //    this.empNo = id;
+        //}
 
         public Employee()
         {
 
-            empNo++;
+        }
+        public Employee(string empName, decimal basicSalary, short deptNo)
+        {
+            id++;
+            this.EmpNo = id;
+            this.EmpName = empName;
+            this.BasicSalary = basicSalary;
+            this.DeptNo = deptNo;
+        }
+        public Employee(string empName, decimal basicSalary)
+        {
+            id++;
+            this.EmpNo = id;
+            this.EmpName = empName;
+            this.BasicSalary = basicSalary;
+           
         }
 
-        void display()
+        public Employee(string empName)
         {
-            Console.WriteLine("Employee No : "+empNo+ " Employee Name : " + empName + " Employee Basic Salary : " + basic + " Employee Dept No : " + deptNo);
+            id++;
+            this.EmpNo = id;
+            this.EmpName = empName;
         }
 
-        static void Main(string[] args)
-        {
-            Employee obj1 = new Employee("Amol", 123465, 10);
-            Employee obj2 = new Employee("Amol", 123465);
-            Employee obj3 = new Employee("Amol");
-            Employee obj4 = new Employee();
 
-            obj1.display();
-            Console.ReadLine();
-            obj2.display(); 
-            Console.ReadLine();
-            obj3.display();
-            Console.ReadLine();
-            obj4.display();
-            Console.ReadLine();
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if (value == null)
+                {
+                    // Console.WriteLine("NOt Invalid Name !!");
+                    empName = value;
+                }
+                else
+                    Console.WriteLine("Invalid Name !!");
+
+
+            }
+        }
+        public int EmpNo
+        {
+            get;
+        }
+
+        public decimal BasicSalary
+        {
+            get { return basicSalary; }
+            set
+            {
+                if (value > 1000 && value < 1000000)
+                    basicSalary = value;
+                else
+                    Console.WriteLine("Invalid salary !!");
+            }
+        }
+        public short DeptNo
+        {
+            get { return deptNo; }
+            set
+            {
+                if (value > 0)
+                    deptNo = value;
+                else
+                    Console.WriteLine("Invalid Depatment Number !!");
+            }
+        }
+
+        public decimal getNetSalary()
+        {
+            double deduction = 1000;
+            decimal salary = basicSalary - (decimal)deduction;
+            return salary;
         }
     }
 }
